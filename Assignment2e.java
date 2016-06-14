ce64c583e92dadaf273ab6298e68859d26ae5e4b
pranavneravetla <pranavneravetla@gmail.com>
2016-06-14 21:52:09 +0530

    Add files via upload

---
+++ Assignment2e.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Assignment2e {
+
+	public static void main(String[] args) {
+		Scanner userinput = new Scanner(System.in);
+		int UserAge  = userinput.nextInt();
+if(UserAge >= 18)
+   {
+	System.out.println("You are allowed to vote");
+   }
+   else
+   { 
+	   System.out.println("You are not allowed to vote");
+   }
+
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
